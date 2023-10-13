220ab62eca72ca66c5adc185315fe2ba2bd828dc
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2023-10-13 23:17:36 +0000

    visualization in r

--- example.R
+++ example.R
@@ -1,3 +1,10 @@
-library(Tmisc)         #nolint
-data(quartet)      #nolint
-view(quartet) 
+# library(Tmisc)         #nolint
+# data(quartet)      #nolint
+# view(quartet) 
+
+#library(ggplot2)
+library(palmerpenguins)
+
+data(penguins)
+View(penguins)
+ggplot(data=penguins)+geom_point((mapping=aes(x=flipper_length_mm,y=body_mass_g))
